bc2082c6ce1309436f15ca265c89efe01cd51915
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-09-14 02:53:08 +0200

    Update basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,1 +1,14 @@
+number1 = 10
+number2 = 5
+#calculate the sum
+sum = number1 + number2
+print(sum)
+
+#calculate the difference
+Subtraction = number1 - number2
+print(Subtraction)
+
+#calculate the product
+Produc t= number1 * number
+print(Product)
 
