274d2da657346a36e2e9731fe2834e671341cb60
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-09-14 03:19:42 +0200

    Update simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,1 +1,10 @@
+principal =1000
+rate = 0.05
+time = 3
 
+#calculate simple interest 
+Interest = principal * rate * time
+Interest = 1000 * 0.05 * 3
+print(Interest)
+
+The simple interest is 150.0
