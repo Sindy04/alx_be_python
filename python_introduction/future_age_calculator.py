2893684832721c6ff2ba5ab9e1671265991443e7
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-09-16 00:00:03 +0200

    Update future_age_calculator.py

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,13 +1,13 @@
 Age = 30
-Age Addition = 27
+Additional = 27
 
 #calculate how old the user will be in the year 2050
 years = 2050 - 2023
 print(years)
 #Addition of years
-Addition = Age + Age Addition
+Addition = Age + Additional
 print(Addition)
 input("How old are you")
-In 2050 = input(",you will be 57 years)
+input("In 2025,you will be 57 years")
                 
 
