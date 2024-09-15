4d59ee8a667cc20abe997ff61fb51db5ccd669c8
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-09-15 23:20:47 +0200

    Update basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -13,6 +13,6 @@ Product = number1 * number
 print(Product)
 
 #outcome
-Addition of 10 and 5 is 15
-Subtraction of 10 and 5 is 5
-Multiplication of 10 and 5 is 50
+Addition = input("Addition of 10 and 5 is 15")
+Subtraction - input("Subtraction of 10 and 5 is 5")
+Multiplication = (" Multiplication of 10 and 5 is 50)
