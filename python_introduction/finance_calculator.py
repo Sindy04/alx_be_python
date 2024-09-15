b9ea5f9fba572cbd47d0308ae618b5b07c0baa4a
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-09-16 00:12:30 +0200

    Update finance_calculator.py

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,1 +1,10 @@
+monthly income = 5000
+monthly expenses = 4000
+monthly savings = 1000
+interest rate = 0.05
+
+#calculate projected savings
+Projected Savings = (monthly savings * 12 +(monthly savings * 12 *0.05))
+print(Projected Savings)
+                     
 
