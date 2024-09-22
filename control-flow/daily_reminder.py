fa00d5b7af6bf79a718bd15f9a90d7f215c69701
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-09-22 09:44:25 +0200

    Update daily_reminder.py

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,14 +1,22 @@
 # Prompt the user for a task description
 task = input("Enter your task: ")
 # Prompt for the task’s priority
-priority = input("priority (high, medium, low): ").lower()
+priority = input("priority (high/medium/low): ").lower()
 # Ask if the task is time-bound
-time-bound = input("is time-bound (yes or no):")
+time-bound = input("is time-bound (yes or no):").lower()
 
 #Intialize
 if time-bound == "yes":
   reminder = f"Reminder: '{task}' is a"
 else:
   reminder =f"Note: '{task} is a'
+
+#Process the task based on priority
+match priority:
+    case "high":
+      reminder += "high priority task"
+    case "medium"
+      reminder += "medium priority task"
+  
   
 
