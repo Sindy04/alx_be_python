fdfacb6a51550b802a1bfaf6ca314fdad3114fa4
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-09-22 11:03:18 +0200

    Update daily_reminder.py

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -2,35 +2,9 @@ print("Enter your task: Finish project report")
 
 # Prompt user input
 Task = input("Enter your Task: ")
-Priority = input("Priority (high/medium/low): ").lower()
-Time_bound = input("Time_bound (yes/no):").lower()
+Priority = input("Priority (high/medium/low): ").strip().lower()
+Time_bound = input(" is itTime_bound? (yes/no):").strip().lower()
 
-#Intialize the reminder message
-reminder_message= "Reminder: "
- 
-if Time_bound == "yes":
-  reminder = f"Reminder: '{Task}' is a"
-else:
-  reminder =f"Note: '{Task} is a'
 
-#Process the task based on priority
-match priority:
-    case "high":
-      reminder += "high priority task"
-    case "medium"
-      reminder += "medium priority task"
-  case "low":
-       reminder += "low priority task"
-  case _: = "Invalid priority level."
-
-  #Modify the reminder
- if time_bound == "yes":
-      reminder += " that requires immediate attention today!"
-else:
-reminder += " Condsider completing it when you have free time. "
-#print the reminder
-print("reminder")
- 
-  
   
 
