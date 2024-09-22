3a0cbbe3f9a06dad6e4b91cad0c66c7a78439d6d
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-09-22 06:57:39 +0200

    Update match_case_calculator.py

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,13 +1,13 @@
  num1 = int(input("Enter the first number:"))
  num2 = int(input("Enter the second number:"))
- operation = input("operation "+, -, *, /":")
-match operation:
-case"+":
-    result = num1 + num2
-  print(f"The result is{result}")
-case"-":
+ operation = input("Choose the operation ("+, -, *, /"):")
+ match operation:
+    case "+":
+      result = num1 + num2
+      print(f"The result is{result}")
+    case "-":
      result = num1 - num2
-   print(f"The result is{result}")
+     print(f"The result is{result}")
 
 
 
