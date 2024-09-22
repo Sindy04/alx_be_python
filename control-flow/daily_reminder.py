35cb0679c2963e8e583acc2a1c75714ca9bc34d2
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-09-22 09:33:09 +0200

    Update daily_reminder.py

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,1 +1,7 @@
+# Prompt the user for a task description
+task = input("Enter your task: ")
+# Prompt for the task’s priority
+priority = input("priority (high, medium, low): ").lower()
+# Ask if the task is time-bound
+time-bound = input("is time-bound (yes or no):")
 
