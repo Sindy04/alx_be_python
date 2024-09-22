b22729219f5f5508046486e725e49a4107d69202
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-09-22 07:10:29 +0200

    Update match_case_calculator.py

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -13,4 +13,14 @@
      result = num1 * num2
      print(f"The result is{result}")
      
+    case "/"
+     result = num1 / num2
+     print(f"The result is{result}") 
+
+    if num2 == 0:
+       print("Cannot divide by zero.")
+    else:
+      result = num1/num2
+      print(result)
+     
 
