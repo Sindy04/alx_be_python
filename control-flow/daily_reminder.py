5709d2b7e966adf81b4e5706af1fca41f449ded3
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-09-24 07:49:28 +0200

    Update daily_reminder.py

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,9 +1,31 @@
-print("Enter your task: Finish project report")
+def get_task_details():
+  "Gets task description, priority and time sensitivity from the user."
+  
+task = input("Enter your task: ")
+priority = input("priority (high/medium/low): ").lower()
+time_bound = input(" is it time-bound? (yes/no):").lower()
+return task, priority, time_bound
 
-# Prompt user input
-Task = input("Enter your Task: ")
-Priority = input("Priority (high/medium/low): ").strip().lower()
-Time_bound = input(" is itTime_bound? (yes/no):").strip().lower()
+def provide_reminder(task, priority,time_bound):
+  "Provides a customizes reminder based on priority and time sensitivity."
+  match priority:
+    case "high":
+      reminder = f"'{task}' is a high priority task"
+    case "medium":
+      reminder = f"'{task}' is a medium priority task"
+    case "low":
+      reminder = f"Note:'{task}' is a low priority task"
+    case:
+      reminder = f"Unknown priority task: '{task}'"
+
+if time_bound == "yes":
+      reminder += " then requires immediate attention today!"
+ elif priority == "low" and time_bound == "no":
+      reminder += ".Consider completing it when you have free time."
+ else:
+      reminder +=" .Complete it as soon as possible."
+   print("Reminder:",reminder)
+
 
 
   
