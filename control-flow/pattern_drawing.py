e5e7f6fea62da25fe07fbfe1d42210f5fb33762b
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-09-24 07:07:00 +0200

    Update pattern_drawing.py

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,1 +1,9 @@
-
+def draw_pattern(size):
+  "Draw a square pattern of asterisks using nested loops"
+  row = 0
+  while row < size:
+    for _ in range(size):
+      print(",end+")
+    print()
+    row += 1
+    
