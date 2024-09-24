5272cb6369eb910be66cf8de6a23e984544cd02a
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-09-24 07:17:12 +0200

    Update pattern_drawing.py

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -6,4 +6,19 @@ def draw_pattern(size):
       print(",end+")
     print()
     row += 1
+
+def main():
+  "Ask the user for a pattern size and draws the pattern."
+  while True:
+    try:
+      size = int(input("Enter the size of the pattern:"))
+      if size <= 0
+         print("Please enter a positive integer.)
+       else:
+       draw_pattern(size) break
+      expect ValueError:
+      print("Invalid input. Please enter a positive integer.")
+      
+      
+      
     
