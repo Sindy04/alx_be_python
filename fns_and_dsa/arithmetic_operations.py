5f52e4571dbc1c9c60e930adc3bfbfe567cbd89a
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-09-26 15:14:21 +0200

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -17,9 +17,9 @@ def perform_operation(num1, num2, operation):
   case "multiply":
     return num1 * num2
   case "divide":
-    if num2 == 0
+    if num2 !== 0
        return num1 / num2
-    else:
+   elif num2 / 0
       return "Error: Division by zero"
    case _:
       return "Error: Invalid operation"
