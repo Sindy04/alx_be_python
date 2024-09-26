6a1434172bef3ec4e48cf23d50d1e56ac1d46aea
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-09-26 16:15:21 +0200

    Update shopping_list_manager.py

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,10 +1,10 @@
 def display_menu():
   """Displays the shopping list manager menu.""""
   print("Shopping List Manager")
-  print("1.Add Item")
-  print("2.Remove Item")
-  print("3.View List")
-  print("4.Exit")
+  print("1. Add Item")
+  print("2. Remove Item")
+  print("3. View List")
+  print("4. Exit")
 
 
 def main():
