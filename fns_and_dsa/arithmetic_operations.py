dd1512168cbfa03947a61a217c0583e5ff4ec19c
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-09-26 14:57:06 +0200

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,11 +1,10 @@
 def perform_operation():
-def perform_operation(num1:float,num2:float,operation: str):
  
 """"
  Args:
    num1 (float): The first number.
    num2 (float): The second number.
-   operation (str): The arithmetic operation to perfom('add', 'subtract', multiply', or 'divide').
+   operation (str): The arithmetic operation to perfom('add', 'subtract', 'multiply', or 'divide').
 
  Returns:
  float or str: The result of the arithmetic operation or an error message for division by zero.
