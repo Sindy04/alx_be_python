2eb4f5718060544befe5ad46fcadad21285060ec
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-10-02 13:24:53 +0200

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -44,7 +44,34 @@ class BankAccount:
     if _name_ == "_main_":
       account = BankAccount(100)
       acount.display_balance()
-      
+
+main-0.py
+import sys
+from bank_account import BankAcount
+
+def main():
+  account = BankAccount(100) #Example starting balance
+  if len(sys.argv) < 2:
+    print("usage: python(programming_paradigm/bank_account.py)<command>:<amount>")
+    print("Command: deposit, withdaw, display")
+    sys.exit(1)
+
+command, params = sys.argv[1].split(':')
+amount = float(paramas[0]) if params else None
+if command == "deposit" and amount is not None:
+  account.depoit(amount)
+  print(f"Deposited: ${amount:.2f}")
+elif command == "withdraw" and amount is not None:
+  if account.withdraw(amount):
+    print(f"Withdraw:${amount:.2f}")
+  else:
+    print("Insufficient funds.")
+  elif command == "display":
+    account.display_balance()
+else:
+print("Invalid command.")
+if _name_ == "-main_"
+main()
       
     
     
