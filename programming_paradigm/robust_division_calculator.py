69a2b869e7f3dbdf523b1b5c260b17ce22b4312e
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-10-02 15:21:21 +0200

    Update robust_division_calculator.py

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,1 +1,21 @@
+def safe_divide(numerator,denominator:
+  """Performs division, handling potential errors.
 
+  Args:
+  numerator(str): Numerator value.
+  denominator(str): Denominator value.
+
+  Returns:
+  str:Result of division or error message.
+  """
+try:
+  numerator = float(numerator)
+  denominator = float(denominator)
+  if denominator == 0:
+    raise Zero DivisionError
+    result = numerator/denominator
+    return f"The result of the division is {result:.2f}"
+except VaplueError:
+  return "Error: Please enter numeric values only."
+except 
+    
