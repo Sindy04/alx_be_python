51b40b7e82035e2e747eb6f8ecd39e34a975f335
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-10-02 18:05:32 +0200

    Update robust_division_calculator.py

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -25,4 +25,11 @@ except Exception as e:
 import sys
 from robust_division_calculator import safe_divide
 def main():
-print("Usage: python
+print("Usage: python(programming_paradigm/robust_division_calculator.py)<numerator><denominator>")
+sys.exit(1)
+numerator = sys.argv[1]
+denominator = sys.argv[2]
+result = safe_divide(numerator, denominator)
+print(result)
+if _name_ =="_main_"
+main()
