a76dc436c8bf6c5a1143935a487a1e744fe1250d
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-10-02 09:14:09 +0200

    Update library_management.py

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -1,1 +1,21 @@
+class Book:
+  def __init__(self,title,author):
+    self.title = title
+    self.author = author
+    self._is_checked_out = false
 
+def check_out(self):
+  self._is_checked_out = True
+
+def return_book(self):
+  self._is_checked_out = False
+
+@property
+def is_available(self):
+  return not self._is_checked_out
+
+class Libriary:
+  def __init__(self):
+    self.books =[]
+
+  def 
