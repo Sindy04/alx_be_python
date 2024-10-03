8fb60999c0ac00e927f51ebcd6dccddd9de34641
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-10-04 01:23:06 +0200

    Update robust_division_calculator.py

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -22,15 +22,9 @@ except ZeroDivisionError:
 except Exception as e:
   return f"An unexpected error occurred: {str(e)}"
 
-main.py
-import sys
-from robust_division_calculator import safe_divide
-def main():
-print("Usage: python(programming_paradigm/robust_division_calculator.py)<numerator><denominator>")
-sys.exit(1)
-numerator = sys.argv[1]
-denominator = sys.argv[2]
-result = safe_divide(numerator, denominator)
-print(result)
-if _name_ =="_main_"
-main()
+class RobustDivisionCalculator:
+  @staticmethod
+  def safe_divide(a,b)
+  if b == 0
+  return None
+  return a/b
