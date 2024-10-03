bf548661c83a115e1cadb4a08a1edf0f8222d6b9
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-10-03 05:00:06 +0200

    Update test_simple_calculator.py

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -1,1 +1,12 @@
+import unittest
+from simple_calculator import SimpleCalculator
 
+Class
+TestSimpleCalculator(unittest.TestCase):
+def setUp(self):
+  self.calculator = SimpleCalculator()
+
+#Test addition
+def test_add_positive_number(self):
+  self.assertEquall(self.calculator.add(2, 3), 5)
+  
