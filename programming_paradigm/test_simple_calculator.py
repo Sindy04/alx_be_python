0b4e886f369b3dfc823711ccb913a81e253930d0
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-10-03 11:00:34 +0200

    Update test_simple_calculator.py

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -7,6 +7,22 @@ def setUp(self):
   self.calculator = SimpleCalculator()
 
 #Test addition
-def test_add_positive_number(self):
-  self.assertEquall(self.calculator.add(2, 3), 5)
+def test_add_positive_numbers(self):
+  self.assertEqual(self.calculator.add(2, 3), 5)
+
+def test_add_negative_numbers(self):
+  self.assertEqual(self.calculator.add(-2,-3),-5)
+
+def test_add_mixed_numbers(self):
+   self.assertEqual(self.calculator.add(-2,3),1)
+
+#Test subtraction
+def test_subtract_positive_numbers(self):
+  self.assertEqual(self.calculator.subtract(5,3),2)
+def test_subtract_negative_numbers(self):
+  self.assertEqual(self.calculator.subtract(-5,-3),-2)
+def test_subtract_mixed_numbers(self):
+  self.assertEqual(self.calculator.subtract(-5,3),-8)
+
+
   
