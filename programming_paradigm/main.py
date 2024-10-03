76ceac443697f9e8fd686e8dae8ecee48224da52
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-10-04 01:15:27 +0200

    Update main.py

--- programming_paradigm/main.py
+++ programming_paradigm/main.py
@@ -1,1 +1,11 @@
-
+import sys
+from robust_division_calculator import safe_divide
+def main():
+print("Usage: python(programming_paradigm/robust_division_calculator.py)<numerator><denominator>")
+sys.exit(1)
+numerator = sys.argv[1]
+denominator = sys.argv[2]
+result = safe_divide(numerator, denominator)
+print(result)
+if _name_ =="_main_"
+main()
