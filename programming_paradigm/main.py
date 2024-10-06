21c181d1bd2d2139606405907a3e810582826b08
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-10-06 07:15:40 +0200

    Update main.py

--- programming_paradigm/main.py
+++ programming_paradigm/main.py
@@ -1,15 +1,11 @@
- File "main.py", line 2, in <module>
-from robust_division_calculator import safe_divide
-import sys
 
-def main():
-a(float):numerator 
-b(float):denominator 
-result =safe_divide(denominator, numerator)
-File"programming_paradigm/robust_division_calculator.py", line 11
+ File "main.py", line 2, in <module>
+    from robust_division_calculator import safe_divide
+  File "/tmp/correction/5600477393056717454255718310459127041156_5/100760/919400/programming_paradigm/robust_division_calculator.py", line 11
     try:
     ^
 SyntaxError: invalid syntax
+
 
    #Test division with non-zero
 result=safe_divide(12,2)
