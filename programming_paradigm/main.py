aafa934c270fb56ab1d871d5d195648d66b3f5df
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-10-06 07:24:43 +0200

    Update main.py

--- programming_paradigm/main.py
+++ programming_paradigm/main.py
@@ -1,18 +1,18 @@
+import sys
+from robust_division_calculator import safe_divide
 
- File "main.py", line 2, in <module>
-    from robust_division_calculator import safe_divide
-  File "/tmp/correction/5600477393056717454255718310459127041156_5/100760/919400/programming_paradigm/robust_division_calculator.py", line 11
-    try:
-    ^
-SyntaxError: invalid syntax
+def main():
+    if len(sys.argv) != 3:
+        print("Usage: python main.py <numerator> <denominator>")
+        sys.exit(1)
 
+    numerator = sys.argv[1]
+    denominator = sys.argv[2]
 
-   #Test division with non-zero
-result=safe_divide(12,2)
-print(f"The result of the division is{result}.") #Output:6.0
-  
-if _name_ == "_main_"
-main()
+    result = safe_divide(numerator, denominator)
+    print(result)
 
+if __name__ == "__main__":
+    main()
 
 
