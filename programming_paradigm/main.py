da8b3f1084c6b36f6f2e96156c4941121ac1f35b
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-10-06 06:38:11 +0200

    Update main.py

--- programming_paradigm/main.py
+++ programming_paradigm/main.py
@@ -1,2 +1,17 @@
+from robust_division_calculator import safe_divide
+import sys
+
+def main():
+if len(sys.argv)!=3:
+  sys.exit(1)
+
+a(float):numerator 
+b(float):denominator 
+result =safe_divide(numerator, denominator)
+ def safe_divide(a,b)
+  if b == 0
+  return None
+  return a / b
+
 
 
