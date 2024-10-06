6a34150932f53754d018abef956e92604663f5c7
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-10-06 06:46:03 +0200

    Update main.py

--- programming_paradigm/main.py
+++ programming_paradigm/main.py
@@ -10,10 +10,6 @@ b(float):denominator
 result =safe_divide(numerator, denominator)
 File"programming_paradigm/robust_division_calculator.py"
 try:
- def safe_divide(a,b)
-  if b == 0
-  return None
-  return a / b
    #Test division with non-zero
 result=safe_divide(12,2)
 print(f"The result of the division is{result}.") #Output:6.0
