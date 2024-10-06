190e545a750d70c2715ea64d8adef9673131ee62
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-10-06 06:05:55 +0200

    Update main.py

--- programming_paradigm/main.py
+++ programming_paradigm/main.py
@@ -1,23 +1,2 @@
-import sys
-from robust_division_calculator import safe_divide
 
-def main():
-#Test division with non_zero divide
-   result = calculator.safe_divide(12,2)
-  print("Result:",result) # Output: 6.0
 
-  
-  calculator = RobustDivisionCalculator()
-  result = calculator.safe_divide(12,2)
-  print(result) # Output: 6.0
-
-  result = calculator.safe_divide(12,0)
-  print(result) #Output: None
-print("Usage: python(programming_paradigm/robust_division_calculator.py)")
-sys.exit(1)
-numerator = sys.argv[1]
-denominator = sys.argv[2]
-result = safe_divide(numerator, denominator)
-print(result)
-if _name_ =="_main_"
-main()
