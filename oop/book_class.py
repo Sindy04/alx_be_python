051a8ea453e7641ac32cea418daf111071871e95
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-10-07 08:44:55 +0200

    Update book_class.py

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,1 +1,36 @@
+class Book:
+  def _init_(self, title, author, year):
+    """
+    Initializes a Book instance with title, author and year.
 
+    Args:
+    title(str): The title of the book.
+    author(str): The author of the book.
+    year(int): The publication year of the book.
+
+
+    """
+    self.title = title
+    self.author = author
+    self.year = year
+
+def _del_(self):
+  """
+  Prints "Deleting (title of the book)" upon object deletion.
+  """
+  def _str_(self):
+  """
+  Returns a string in the format "(title) by (author), published in (year)".
+
+  Returns:
+  str: A human-readable representation of the Book instance.
+  """
+  return f"{self.title} by {self.author}, published in {self.year}"
+
+def _repr_(self):
+  Returns a string that would recreate the Book instance.
+
+    Returns:
+  str: An official representation of the Book instance.
+  """
+  return f"Book('{self.title}','{self.author}',{self.year})"
