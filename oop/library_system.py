5d2392741b8243ba501dac49bf47c070b3999e6c
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-10-07 12:09:00 +0200

    Update library_system.py

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,1 +1,27 @@
+class Book:
+  def __init__(self,title,author):
+    """
+    Initilizes a Book instance with title and author.
 
+    Args:
+    title(str): The title of the book.
+    author(str): The author of the book.
+    """
+    self.title = title
+    self.author = author
+
+def __str__(self):
+  """
+  Returns a string representation of the Book instance.
+  Returns:
+  str: A human-readable representation of the Book instance.
+  """
+  return f"Book: {self.title} by {self.author}"
+
+class EBook(/Book):
+  def __int__(self,title,author,file_size):
+    """
+    Initializes an EBook instance with title,author and file_size
+
+    Args:
+  
