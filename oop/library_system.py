f1dfb450979083e3742e89c20d3c90282a3aa8e7
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-10-07 13:39:54 +0200

    Update library_system.py

--- oop/library_system.py
+++ oop/library_system.py
@@ -24,4 +24,29 @@ class EBook(/Book):
     Initializes an EBook instance with title,author and file_size
 
     Args:
+    title (str): The title of the book.
+    author (str): The author of the book.
+    file_size (int): The file size of the eBook in KB.
+    """
+    super()__init__(title,author)
+    self.file_size = file_size
+
+def __str__(self):
+  """
+  Reurns a strong representation of the EBook instance.
+
+  Returns:
+  str: A human-readable representation of the EBook instance.
+  """
+  return f"EBook:{self.title} by {self.author}, File Size: {self.file_size}KB"
+
+class PrintBook(Book):
+  def __init__(self,title,author,page_count):
+    """
+    Initializes a PrintBook instance with title,author and page count.
+
+    Args:
+    title (str): The title of the book.
+    author (str): The author of the book.
+    page_count (int): The page count of the print book.
   
