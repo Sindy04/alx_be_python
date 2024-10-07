eb2be12216bf06b09080d04727648701af6150e9
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-10-07 09:34:20 +0200

    Update class_static_methods_demo.py

--- oop/class_static_methods_demo.py
+++ oop/class_static_methods_demo.py
@@ -1,1 +1,33 @@
+class Calculator:
+  """
+  A class for performing arithmetic operations.
+  """
+  calculation_type = "Arithmetic Operations"
 
+@staticmethod
+def add(a,b):
+  """
+  Returns the sum of two numbers.
+
+Args:
+a (int or float): The first number.
+b (int or float): The second number.
+
+Returns:
+int or float: The sum of a and b.
+"""
+  return a + b
+  @classmethod
+  def multiply(cls, a, b):
+    """
+    Returns the product of two numbers.
+
+    Args:
+    a (int or float): The first number
+    b (int or float): The second number
+
+    Returns:
+    int or float: The product of a and b.
+    """
+    print(f"Calculation type: {cls.calculation_type}")
+    return a * b
