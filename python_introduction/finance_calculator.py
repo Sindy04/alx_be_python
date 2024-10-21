b46965bb7866742563d9c881dee579b8573522e9
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-10-21 10:59:25 +0200

    Update finance_calculator.py

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -7,4 +7,9 @@ interest rate = 0.05
 Projected Savings = (monthly savings * 12 +(monthly savings * 12 *0.05))
 print(Projected Savings)
                      
+def calculate_monthly_savings(monthly_income, monthly_expenses):
+  return monthly_income - monthly_expenses
 
+def calculate_projected_annual_savings(monthly_savings):
+  annual_interest_rate = 0.05 #5% annual interest rate
+ return monthly_savings * 12 + (monthly_savings *12 *annual_ interest_rate)
