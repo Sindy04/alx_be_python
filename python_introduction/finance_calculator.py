9a42cec2bbab02d1a0a59c0127cbc651d43a008b
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-10-21 11:12:07 +0200

    Update finance_calculator.py

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,12 +1,4 @@
-monthly income = 5000
-monthly expenses = 4000
-monthly savings = 1000
-interest rate = 0.05
-
-#calculate projected savings
-Projected Savings = (monthly savings * 12 +(monthly savings * 12 *0.05))
-print(Projected Savings)
-                     
+                
 def calculate_monthly_savings(monthly_income, monthly_expenses):
   return monthly_income - monthly_expenses
 
@@ -16,8 +8,8 @@ def calculate_projected_annual_savings(monthly_savings):
 
 def main():
   #User input for financial details
-  monthly_income = input("Enter your monthly income:$"):
-  monthly_expenses = input("Enter your total monthly expenses: $"):
+  monthly_income = input("Enter your monthly income:$")
+  monthly_expenses = input("Enter your total monthly expenses: $")
 
 #Calculate monthly savings
 monthly_savings = calculate_monthly_saving(monthly_income, monthly_expenses)
