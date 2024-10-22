f04102bfde65615f5dae880604449e58a4f1177f
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-10-22 10:58:40 +0200

    Update rectangle_area.py

--- python_introduction/rectangle_area.py
+++ python_introduction/rectangle_area.py
@@ -3,5 +3,6 @@ width = 5
 
 #Calculate area of rectangle
 area = length * width
-area =10 * 5 The area of the rectangle is: 50
+area =10 * 5 
+print(f"The area of the rectangle is: 50")
 print(area)
